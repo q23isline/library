9e7e16a3497e8f6be837b9c0475b7def9e7996fa
q23isline <rowpure@gmail.com>
2025-08-27 22:03:43 +0900

    feat: 本CRUDのMVCを生成 ```bash docker compose exec app bin/cake bake all books sudo chmod -R ugo+rw app ```

---
+++ app/src/Model/Entity/Book.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Book Entity
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $author
+ * @property \Cake\I18n\DateTime $created
+ * @property \Cake\I18n\DateTime $modified
+ */
+class Book extends Entity
+{
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array<string, bool>
+     */
+    protected array $_accessible = [
+        'name' => true,
+        'author' => true,
+        'created' => true,
+        'modified' => true,
+    ];
+}
